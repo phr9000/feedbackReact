7718b1a383f8fb1377419bcc5e3674db0e6cfa71
hailan <phr9000@gmail.com>
2022-11-23 17:15:10 +0900

    13강까지

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ]
-    return (
-        <div className="container">
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
+import Header from "./components/Header"
 
-            <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {comments.map((comment, index) =>(
-                        <li key={index}>{comment.text}</li>
-                    ))}
-                </ul>
+function App() {
+    return (
+        <>
+            <Header/>
+            <div className="container">
+                <h1>My App</h1>
             </div>
-        </div>
+        </>    
         )
 }
 
