922639c8f6b88027230bf3a7a3280c0acd9d45e1
hailan <phr9000@gmail.com>
2023-05-02 20:00:49 +0900

    context전 29까지

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,21 @@
+import Card from '../components/shared/Card'
+import {Link} from 'react-router-dom'
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className='about'>
+        <h1>About This Project</h1>
+        <p>This is a Reat app to leave Feedback
+        for a product or service</p>
+        <p>Version: 1.0.0</p>
+
+        <p>
+          <Link to='/'>Back To Home</Link>
+        </p>
+      </div>
+    </Card>
+  )
+}
+
+export default AboutPage
